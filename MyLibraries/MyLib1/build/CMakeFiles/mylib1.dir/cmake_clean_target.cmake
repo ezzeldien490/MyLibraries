d551d1745f113c2b07eae2559313621926fc5670
ezzeldien490 <ezzeldien490@gmail.com>
2024-12-10 17:45:03 +0200

    test

--- MyLibraries/MyLib1/build/CMakeFiles/mylib1.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "E:/MyLibraries/MyLib1/lib/libmylib1.a"
-)
---
+++ build/CMakeFiles/mylib1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib1.dir/src/MyReadLib.cpp.obj"
+  "CMakeFiles/mylib1.dir/src/MyReadLib.cpp.obj.d"
+  "E:/Programming/MyLibraries/MyLibraries/MyLib1/lib/libmylib1.a"
+  "E:/Programming/MyLibraries/MyLibraries/MyLib1/lib/libmylib1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylib1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
